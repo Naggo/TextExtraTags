2293371421536c8e46b29efb76371da91a5daf41
Naggo <107308836+Naggo@users.noreply.github.com>
2024-09-13 22:28:59 +0900

    Add TextExtraParser

--- src/TextExtraTags/Runtime/TextExtraParser.cs
+++ src/TextExtraTags/Runtime/TextExtraParser.cs
@@ -1,17 +1,56 @@
+#if TEXTEXTRATAGS_TEXTMESHPRO_SUPPORT
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 namespace TextExtraTags {
+    [RequireComponent(typeof(TMP_Text))]
     public class TextExtraParser : MonoBehaviour {
+        [TextArea(5, 10)]
+        public string sourceText;
 
-        void Awake() {
-        
+        public string parserName;
+        public bool parseOnAwake;
+
+        ExtraTagCollection _extraTags;
+        ExtraTagCollection extraTags {
+            get {
+                if (_extraTags is null) {
+                    _extraTags = new();
+                }
+                return _extraTags;
+            }
+        }
+        public IReadOnlyExtraTagCollection ExtraTags => extraTags;
+
+        TMP_Text _textComponent;
+        public TMP_Text TextComponent {
+            get {
+                if (_textComponent is null) {
+                    _textComponent = GetComponent<TMP_Text>();
+                }
+                return _textComponent;
+            }
         }
 
-        void Update() {
-        
+
+        void Awake() {
+            if (parseOnAwake) {
+                Parse();
+            }
+        }
+
+        public ReadOnlySpan<char> Parse() {
+            var parser = ParserUtility.GetParser(parserName);
+            var buffer = parser.Parse(sourceText, extraTags).AsArraySegment();
+            TextComponent.SetCharArray(buffer.Array, buffer.Offset, buffer.Count);
+            return parser.AsSpan();
         }
     }
 }
+
+#endif
