e7ac59d3a0ec40161427e14946ad84e3050b34a1
Naggo <naggo.plant@gmail.com>
2024-09-15 13:47:42 +0900

    Update SettingsProvider

--- src/TextExtraTags/Editor/TextExtraTagsSettingsProvider.cs
+++ src/TextExtraTags/Editor/TextExtraTagsSettingsProvider.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Compilation;
 
 
 namespace TextExtraTags.Editor {
@@ -40,6 +42,8 @@ namespace TextExtraTags.Editor {
 
 
         public override void OnGUI(string searchContext) {
+            DrawSymbolInfo();
+
             if (_editor?.target == null) {
                 Editor.CreateCachedEditor(TextExtraTagsSettings.Instance, null, ref _editor);
             }
@@ -60,6 +64,31 @@ namespace TextExtraTags.Editor {
                 EditorGUILayout.Space();
                 _editor.OnInspectorGUI();
             }
+        }
+
+        void DrawSymbolInfo() {
+#if !TEXTEXTRATAGS_ZSTRING_SUPPORT
+            string path = CompilationPipeline
+                .GetAssemblyDefinitionFilePathFromAssemblyName("ZString");
+            if (string.IsNullOrEmpty(path)) {
+                return;
+            }
+
+            EditorGUILayout.HelpBox(
+                "ZString.asmdef detected. "+
+                "We recommend adding the TEXTEXTRATAGS_ZSTRING_SUPPORT Symbol.",
+                MessageType.Info, true
+            );
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PrefixLabel(" ");
+            if (GUILayout.Button("Add Sctipting Define Symbols")) {
+                var btGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+                var btNamed = NamedBuildTarget.FromBuildTargetGroup(btGroup);
+                PlayerSettings.SetScriptingDefineSymbols(btNamed, "TEXTEXTRATAGS_ZSTRING_SUPPORT");
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+#endif
         }
     }
 }
